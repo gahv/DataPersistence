b729d1423c515b912b6148ea815e77cef1252a06
Gorky Velásquez <gorkyandre@gmail.com>
2020-09-15 00:22:14 -0300

    inclusa verificação de alteração de informações de conexão no método Connection::getInstance, incluso metodo para retornar a mensagem de erro formatada

--- src/Connection.php
+++ src/Connection.php
@@ -34,14 +34,19 @@ class Connection
      */
     public static function getInstance(?string $db_user = "", ?string $db_password = "", ?string $db_name = ""): ?PDO
     {
+        $currentConn = hash("md5", self::$db_user . self::$db_password . self::$db_name);
+        $customConn = hash("md5", $db_user . $db_password . $db_name);
+
+        // Check changes in connection information
+        $newinstance = ($currentConn != $customConn);
+
         self::$db_user     = ($db_user) ? $db_user : DB_CONFIG["username"];
         self::$db_password = ($db_password) ? $db_password : DB_CONFIG["passwd"];
         self::$db_name     = ($db_name) ? $db_name : DB_CONFIG["dbname"];
 
         $conn_string = self::getConnectionString(self::$db_name);
 
-        // Check connection string changes
-        if (self::$connection_string != $conn_string) {
+        if ($newinstance) {
             try {
                 self::$instance = new PDO(
                     $conn_string,
@@ -101,7 +106,7 @@ class Connection
         switch (DB_CONFIG["driver"]) {
             case 'sqlsrv':
                 self::$connection_string =
-                    DB_CONFIG["driver"] . ":Server=" . DB_CONFIG["host"] . "," . DB_CONFIG["port"] . ";Database=" . $db_name . ";WSID=" . DB_CONFIG["appname"] . ";";
+                    DB_CONFIG["driver"] . ":Server=" . DB_CONFIG["host"] . "," . DB_CONFIG["port"] . ";Database=" . $db_name . ";app=" . DB_CONFIG["appname"] . ";";
                 break;
             default:
                 self::$connection_string =
@@ -109,5 +114,24 @@ class Connection
         }
 
         return self::$connection_string;
+    }
+
+    /**
+     * Formats error message
+     */
+    public static function getErrorMessage(): string
+    {
+        $error = "";
+
+        switch (DB_CONFIG["driver"]) {
+            case 'sqlsrv':
+                $error = self::$error->getMessage();
+                $error = substr($error, strripos($error, '[SQL Server]') + 12);
+                break;
+            default:
+                $error = self::$error->getMessage();
+        }
+
+        return $error;
     }
 }
